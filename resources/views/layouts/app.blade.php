9851b39cae0579a756461558766c8eca09e9bddf
Morra13 <55277914+Morra13@users.noreply.github.com>
2023-11-03 16:24:47 +0400

    #2 CSS init + Fonts + favicon

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? config('app.name', 'title') }}</title>
+
+    <link rel="stylesheet" href="{{ asset('storage') }}/css/normalize.css">
+    <link rel="stylesheet" href="{{ asset('storage') }}/css/style.css">
+
+    <link type="image/png" sizes="16x16" rel="icon" href="{{ asset('storage') }}/favicon.svg">
+</head>
+<body>
+
+</body>
+</html>
