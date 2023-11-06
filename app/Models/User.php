fffc0c06eedac9645eb2e88b5b2b9e3c8949cb0e
Morra13 <55277914+Morra13@users.noreply.github.com>
2023-11-06 12:01:30 +0400

    #10 user Model,Migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,33 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ * @package App
+ *
+ * @property int id
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * @property string role user|admin
+ * @property string email
+ * @property string password
+ * @property string remember_token
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The table associated with the model.
+     */
+    const TABLE = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +59,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Check is admin
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return $this->role == 'admin';
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create(\App\Models\User::TABLE, function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
