c6d45796c20adfde1e1f1a3172fb3be7ad2c1c61
Morra13 <55277914+Morra13@users.noreply.github.com>
2023-11-07 22:08:00 +0400

    #16 create product

--- www/app/Http/Controllers/Api/ProductController.php
+++ www/app/Http/Controllers/Api/ProductController.php
@@ -21,9 +21,32 @@ class ProductController extends Controller
     /** @var string */
     const ROUTE_DELETE = 'api.product.delete';
 
-    public function create ()
+    /**
+     * Создание продукта
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function create (Request $request)
     {
         $obProduct = new Product();
+
+        $arrKeys = explode(',' , substr($request->get('inputArrKeys'), 0, -1));
+
+        foreach ($arrKeys as $value) {
+            $arrJson[] = [
+                $request->get('attributeName_'.$value) => $request->get('attributeValue_'.$value),
+            ];
+        }
+
+        $json = json_encode($arrJson);
+        $obProduct->article = $request->get('article');
+        $obProduct->name = $request->get('name');
+        $obProduct->status = $request->get('status');
+        $obProduct->data = $json;
+        $obProduct->save();
+
+        return redirect()->route(\App\Http\Controllers\PublicController::ROUTE_INDEX);
     }
 
     public function update ()
--- www/resources/views/layouts/add.blade.php
+++ www/resources/views/layouts/add.blade.php
@@ -1,14 +1,14 @@
 <div class="add__wrapper_darkening">
     <div class="add__wrapper_popup">
-        <form action="/" class="add__form flex">
+        <form method="post"  action="{{ route(\App\Http\Controllers\Api\ProductController::ROUTE_CREATE) }}" class="add__form flex">
             <button type="button" class="add__exit_link btn-reset">
                 <img src="{{ asset('storage') }}/img/exit.svg" alt="{{ __('main.exit') }}" class="add__exit">
             </button>
             <h2 class="add__title">{{ __('main.addProduct') }}</h2>
             <label for="article" class="add__label">{{ __('main.article') }}</label>
-            <input type="text" class="add__input input-reset" id="article">
+            <input type="text" class="add__input input-reset" id="article" name="article">
             <label for="name" class="add__label">{{ __('main.name') }}</label>
-            <input type="text" class="add__input input-reset" id="name">
+            <input type="text" class="add__input input-reset" id="name" name="name">
             <label for="status" class="add__label flex">{{ __('main.status') }}</label>
             <div class="dropdown">
                 <button class="dropdown__button" name="status" type="button">{{ __('main.available') }}</button>
@@ -21,10 +21,11 @@
             <h3 class="add__subtitle">{{ __('main.attributes') }}</h3>
             <div class="add__link_div">
                 <div id="attributes"></div>
+                <input type="hidden" id="inputArrKeys" name="inputArrKeys" value="">
                 <button type="button" onclick="addDiv()" class="add__link btn-reset">{{ __('main.addAttribute') }}</button>
             </div>
             <div>
-                <button class="add__btn btn-reset">{{ __('main.add') }}</button>
+                <button type="submit" class="add__btn btn-reset">{{ __('main.add') }}</button>
             </div>
         </form>
     </div>
@@ -34,13 +35,32 @@
 
     function addDiv() {
         i++;
-        var newdiv = document.createElement("div");
-        newdiv.innerHTML = "<div class='add__attributes flex' id='attributes"+i+"'><div class='add__name flex'><label for='name"+i+"' class='add__label'>{{ __('main.name') }}</label><input type='text' class='add__input add__input_attributes input-reset' id='name"+i+"'></div><div class='add__value flex'><label for='value"+i+"' class='add__label'>{{ __('main.value') }}</label><input type='text' class='add__input add__input_attributes input-reset' id='value"+i+"'></div><a href='#' onclick='deleteDiv(attributes"+i+")' class='add__trash'><img src='{{ asset('storage') }}/img/trash.svg' alt='{{ __('main.delete') }}' class='add__trash_img'></a></div>";
+        let newdiv = document.createElement("div");
+        newdiv.innerHTML = "<div class='add__attributes flex' id='attributes_"+i+"'><div class='add__name flex'><label for='attributeName_"+i+"' class='add__label'>{{ __('main.name') }}</label><input type='text' class='add__input add__input_attributes input-reset' id='attributeName_"+i+"' name='attributeName_"+i+"'></div><div class='add__value flex'><label for='attributeValue_"+i+"' class='add__label'>{{ __('main.value') }}</label><input type='text' class='add__input add__input_attributes input-reset' id='attributeValue_"+i+"' name='attributeValue_"+i+"'></div><a href='#' onclick='deleteDiv(attributes_"+i+")' class='add__trash'><img src='{{ asset('storage') }}/img/trash.svg' alt='{{ __('main.delete') }}' class='add__trash_img'></a></div>";
         document.getElementById("attributes").appendChild(newdiv);
+
+        function keyGen (count) {
+            arrRes = [];
+            for (y = 0; y < count; ++y) {
+                arrRes.push( y + 1 );
+            }
+            return arrRes;
+        }
+
+        let arrKeys = keyGen(i);
+        let sKeys = arrKeys.join(',')+',';
+        const inputArrKeys = document.querySelector('#inputArrKeys');
+        inputArrKeys.value = sKeys;
+
         return false;
     }
 
     function deleteDiv(div) {
+        const sliceLength = 11;
+        const inputArrKeys = document.querySelector('#inputArrKeys');
+        let key = div.id.slice(sliceLength);
+        let resultKeys = inputArrKeys.value.replace(key+',','');
+        inputArrKeys.value = resultKeys;
         div.remove()
         return false;
     }
